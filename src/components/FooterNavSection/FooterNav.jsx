51472cba029e65db2a12c79ddc63de0af309e89e
Terna Nanev <ternathompson2@gmail.com>
2025-01-31 15:49:15 +0100

    modified footer

--- src/components/FooterNavSection/FooterNav.jsx
+++ src/components/FooterNavSection/FooterNav.jsx
@@ -10,8 +10,8 @@ import youtubeIcon from "../../assets/youtubeIcon.svg";
 function FooterNav() {
   return (
     <section className="w-full flex flex-col justify-center px-30 py-10 sm:px-25 space-y-4 max-sm:px-10 items-center">
-      <div className="grid grid-cols-5 w-full space-y-10 border-0">
-        <div className="flex flex-col space-y-4">
+      <div className="w-full flex lg:flex-row md:flex-row max-sm:flex-col sm:flex-col sm:space-y-4 max-sm:space-y-4 border-0">
+        <div className="lg:w-80 flex flex-col space-y-4 border-0">
           <div className="flex flex-row items-center space-x-2">
             <img src={Logo} alt="logo" className="w-8" />
             <h1 className="w-ful font-bold text-sm">Pay4Me App</h1>
@@ -31,40 +31,42 @@ function FooterNav() {
           </div>
         </div>
 
-        <div className="flex flex-col space-y-4">
-          <h1 className="font-semibold">Company</h1>
-          <span className="text-xs text-slate-500">About</span>
-          <span className="text-xs text-slate-500">Blog</span>
-          <span className="text-xs text-slate-500">How it works</span>
-          <span className="text-xs text-slate-500">Careers</span>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <h1>Resources</h1>
-          <span className="text-xs text-slate-500">Android Reviews</span>
-          <span className="text-xs text-slate-500">IOS Reviews</span>
-          <span className="text-xs text-slate-500">Testimonials</span>
-          <span className="text-xs text-slate-500">FAQs</span>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <h1>Legal</h1>
-          <span className="text-xs text-slate-500">Terms of Service</span>
-          <span className="text-xs text-slate-500">Terms of Service</span>
-          <span className="text-xs text-slate-500">Privacy Policy</span>
-          <span className="text-xs text-slate-500">KYC/AML Policy</span>
-        </div>
+        <div className="w-full flex flex-row max-sm:grid max-sm:grid-cols-2 max-sm:space-y-4 space-x-10">
+          <div className="w-full flex flex-col space-y-4">
+            <h1 className="font-semibold">Company</h1>
+            <span className="text-xs text-slate-500">About</span>
+            <span className="text-xs text-slate-500">Blog</span>
+            <span className="text-xs text-slate-500">How it works</span>
+            <span className="text-xs text-slate-500">Careers</span>
+          </div>
+          <div className="w-full flex flex-col space-y-4">
+            <h1>Resources</h1>
+            <span className="text-xs text-slate-500">Android Reviews</span>
+            <span className="text-xs text-slate-500">IOS Reviews</span>
+            <span className="text-xs text-slate-500">Testimonials</span>
+            <span className="text-xs text-slate-500">FAQs</span>
+          </div>
+          <div className="w-full flex flex-col space-y-4">
+            <h1>Legal</h1>
+            <span className="text-xs text-slate-500">Terms of Service</span>
+            <span className="text-xs text-slate-500">Terms of Service</span>
+            <span className="text-xs text-slate-500">Privacy Policy</span>
+            <span className="text-xs text-slate-500">KYC/AML Policy</span>
+          </div>
 
-        <div>
-          {/* <img src={DLButtons} alt="download-buttons" className="w-full" /> */}
-          <span className="w-full text-xs text-slate-500">
-            Depending on your mobile device, download our awesome native app to
-            start enjoying all the benefits of pay4Me.
-          </span>
+          <div className="w-full">
+            {/* <img src={DLButtons} alt="download-buttons" className="w-full" /> */}
+            <span className="w-full text-xs text-slate-500">
+              Depending on your mobile device, download our awesome native app
+              to start enjoying all the benefits of pay4Me.
+            </span>
+          </div>
         </div>
       </div>
 
       <div className="text-sm text-slate-500">© 2020 - 2024 Pay4Me, Inc.</div>
 
-      <div className="flex flex-row space-x-2 ">
+      <div className="max-sm:full flex flex-row space-x-2 ">
         <img src alt="logo-bottom" width={80} />
         <span className="text-xs text-slate-500">
           Pay4Me is a financial technology company making it easier for
